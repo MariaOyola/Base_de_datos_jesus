06d000260d6f6670bc42efbed3b1aa12ca77c3ec
Maria Jose <mariajoserodriguezoyola@gmaail.com>
2026-04-18 20:09:53 -0500

    subo HU-01-dev

--- 02_dml/00_inserts/001_insert_geography.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO geography.time_zone 
-(time_zone_name, utc_offset_minutes, created_by, updated_by)
-VALUES
-('UTC', 0, gen_random_uuid(), gen_random_uuid()),
-('America/Bogota', -300, gen_random_uuid(), gen_random_uuid()),
-('America/New_York', -300, gen_random_uuid(), gen_random_uuid()),
-('Europe/Madrid', 60, gen_random_uuid(), gen_random_uuid()),
-('Asia/Tokyo', 540, gen_random_uuid(), gen_random_uuid()),
-('Australia/Sydney', 600, gen_random_uuid(), gen_random_uuid());
